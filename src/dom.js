b2b1bc0cc7708a4abed155a69060bacd918272f6
KG <karlgustav.ek@hotmail.com>
2023-03-02 13:29:12 +0100

    added icons, fixed media query and other styling.

--- src/dom.js
+++ src/dom.js
@@ -9,20 +9,20 @@ const searchPage = document.getElementById('searched-pokemon');
 const teamPage = document.getElementById('team-pokemon');
 
 function togglePage(page_id) {
-    if(page_id === "team") {
+    if (page_id === "team") {
         teamPage.style.display = 'block';
         searchPage.style.display = 'none';
     }
-    if(page_id === "search") {
+    if (page_id === "search") {
         teamPage.style.display = 'none';
         searchPage.style.display = 'block';
     }
 }
 
-searchButton.addEventListener("click", () => {togglePage("search")});
-teamButton.addEventListener("click", () => {togglePage("team")});
+searchButton.addEventListener("click", () => { togglePage("search") });
+teamButton.addEventListener("click", () => { togglePage("team") });
 
-function pokecard_factory(pokemon, card_type, in_reserve=false) {
+function pokecard_factory(pokemon, card_type, in_reserve = false) {
     let pokecard = document.createElement("div");
 
     let pokename = document.createElement("div");
@@ -37,16 +37,16 @@ function pokecard_factory(pokemon, card_type, in_reserve=false) {
     pokecard.append(poke_img);
 
     const ability_container = document.createElement("div");
-
-    for(const ability of pokemon.abilities) {
+    ability_container.innerText = "Abilites:"
+    for (const ability of pokemon.abilities) {
         const ability_div = document.createElement("div");
-        ability_div.innerText += ability.ability.name;
+        ability_div.innerText += ability.ability.name + '.';
         ability_container.appendChild(ability_div);
     }
 
     pokecard.append(ability_container);
 
-    if(card_type === "search" || card_type === "reserve") {
+    if (card_type === "search" || card_type === "reserve") {
         let pokebutton = document.createElement("button");
         pokebutton.innerText = "Add To Team"
         pokebutton.classList = "addteam"
@@ -55,34 +55,34 @@ function pokecard_factory(pokemon, card_type, in_reserve=false) {
 
         // Event för addering till Team
         pokebutton.addEventListener("click", function () {
-            if(team_container.children.length < 3) {
+            if (team_container.children.length < 3) {
                 team_container.append(pokecard_factory(pokemon, "team"));
-        
+
                 // Remove from reserve container if in there
                 if (in_reserve) {
                     pokecard.remove();
                 }
             }
             else {
-                if(pokecard.classList.contains("FullMessage")) { return; }
-        
+                if (pokecard.classList.contains("FullMessage")) { return; }
+
                 let messageElement = document.createElement("p");
                 messageElement.innerText = "Team is full";
                 messageElement.classList = "Teamfull"
                 pokecard.classList.add("FullMessage");
                 pokecard.append(messageElement);
-        
+
                 // Timer för "Team Full i ms"
-                setTimeout(function() {
+                setTimeout(function () {
                     pokecard.classList.remove("FullMessage");
                     messageElement.remove();
                 }, 300);
             }
         });
-        
+
     }
 
-    if(card_type === "search") {
+    if (card_type === "search") {
         let reservebutton = document.createElement("button");
         reservebutton.innerText = "Add To Reserve"
         reservebutton.classList = "addreserve"
@@ -95,7 +95,7 @@ function pokecard_factory(pokemon, card_type, in_reserve=false) {
         });
     }
 
-    if(card_type === "team" || card_type === "reserve") {
+    if (card_type === "team" || card_type === "reserve") {
         let kickButton = document.createElement("button");
         kickButton.innerText = "Kick";
         kickButton.classList = "kick"
@@ -106,46 +106,59 @@ function pokecard_factory(pokemon, card_type, in_reserve=false) {
         pokecard.append(kickButton);
     }
 
-    if(card_type === "team") {
+    if (card_type === "team") {
+        // create parent div for buttons
+        let buttonsDiv = document.createElement("div");
+        buttonsDiv.classList = "buttons-container";
+        
+        // create up button
         let upButton = document.createElement("button");
-        upButton.innerText = "Up";
         upButton.classList = "up";
-        upButton.addEventListener("click", function() {
+        upButton.addEventListener("click", function () {
             let prevSibling = pokecard.previousElementSibling;
             if (prevSibling !== null) {
                 team_container.insertBefore(pokecard, prevSibling);
             }
         });
-
+      
+        // create down button
         let downButton = document.createElement("button");
-        downButton.innerText = "Down";
         downButton.classList = "down";
-        downButton.addEventListener("click", function() {
+        downButton.addEventListener("click", function () {
             let nextSibling = pokecard.nextElementSibling;
             if (nextSibling !== null) {
                 team_container.insertBefore(nextSibling, pokecard);
             }
         });
-        pokecard.append(upButton);
-        pokecard.append(downButton);
-
+      
+        // append buttons to the parent div
+        buttonsDiv.appendChild(upButton);
+        buttonsDiv.appendChild(downButton);
+      
+        // append parent div to the pokecard element
+        pokecard.appendChild(buttonsDiv);
+      
+        // create rename input and button
         let renameInput = document.createElement("input");
         renameInput.type = "text";
-
+        renameInput.classList = "rename-input";
+      
         let renameSubmit = document.createElement("button");
-        renameSubmit.classList = "renameButton"
-        renameSubmit.innerText = "Rename"
+        renameSubmit.classList = "renameButton";
+        renameSubmit.innerText = "Rename";
         renameSubmit.addEventListener("click", () => {
-            if(renameInput.value != "") {
+            if (renameInput.value != "") {
                 pokename.innerText = renameInput.value;
             }
             renameInput.value = "";
         });
-
+      
+        // append rename input and button to the pokecard element
         pokecard.append(renameInput);
         pokecard.append(renameSubmit);
-    } 
-        
+      }
+      
+
 
 
     return pokecard;
